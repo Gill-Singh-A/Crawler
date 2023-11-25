256958425c344e74396a565852c62f7dacf9753c
Gill-Singh-A <amansg22@iitk.ac.in>
2023-11-25 03:26:15 +0000

    Added Command Line Agrument to Crawl External URLs

--- crawler.py
+++ crawler.py
@@ -33,20 +33,25 @@ def get_arguments(*args):
 		parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
 	return parser.parse_args()[0]
 
-internal_urls, external_urls, interested_url = [], [], []
+internal_urls, external_urls, all_urls, interested_url = [], [], [], []
 total_visited = 0
 done = []
+crawl_external = False
 
 def is_valid_url(url):
 	parsed = urlparse(url)
 	return bool(parsed.netloc) and bool(parsed.scheme)
 def get_all_urls(url):
 	domain_name = urlparse(url).netloc
-	response = requests.get(url, headers=headers)
+	try:
+		response = requests.get(url, headers=headers)
+	except:
+		display(':', f"Error while Crawling URL {Back.MAGENTA}{url}{Back.RESET}")
+		return -1
 	try:
 		soup = BeautifulSoup(response.content, "html.parser")
 	except:
-		display(':', f"Error while Parsing {Back.MAGENTA}{url}{Back.RESET}")
+		display(' ', f"Error while Parsing {Back.MAGENTA}{url}{Back.RESET}")
 		return -1
 	for a_tag in soup.findAll("a"):
 		href = a_tag.attrs.get("href")
@@ -63,11 +68,13 @@ def get_all_urls(url):
 			if href not in external_urls:
 				display(':', f"External Link => {Back.MAGENTA}{href}{Back.RESET}")
 				external_urls.append(href)
+				all_urls.append(href)
 			continue
 		display('+', f"Internal Link => {Back.MAGENTA}{href}{Back.RESET}")
 		internal_urls.append(href)
+		all_urls.append(href)
 	return response.text
-def crawl(url, interests):
+def crawl(url, interests, external):
 	global total_visited
 	total_visited += 1
 	try:
@@ -83,12 +90,20 @@ def crawl(url, interests):
 				else:
 					interested_url.append(url)
 					display('-', f"Interested URL => {Back.MAGENTA}{url}{Back.RESET}")
-			if i < len(internal_urls)-1:
-				while internal_urls[i] in done:
-					i += 1
-				url = internal_urls[i]
+			if external == False:
+				if i < len(internal_urls)-1:
+					while internal_urls[i] in done and i < len(internal_urls)-1:
+						i += 1
+					url = internal_urls[i]
+				else:
+					break
 			else:
-				break
+				if i < len(all_urls)-1:
+					while all_urls[i] in done and i < len(all_urls)-1:
+						i += 1
+					url = all_urls[i]
+				else:
+					break
 	except KeyboardInterrupt:
 		return len(internal_urls) + len(external_urls)
 	return len(internal_urls) + len(external_urls)
@@ -97,7 +112,8 @@ if __name__ == "__main__":
 	data = get_arguments(('-u', "--url", "url", "URL to start Crawling from"),
                          ('-f', "--find", "find", "Words to find (seperated by ',')"),
                          ('-s', "--session-id", "session_id", "Session ID (Cookie) for the Request Header (Optional)"),
-                         ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"))
+                         ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"),
+						 ('-e', "--external", "external", f"Crawl on External URLs (True/False, default={crawl_external})"))
 	if not data.url:
 		display('-', "Please specify a URL!")
 		exit(0)
@@ -106,11 +122,16 @@ if __name__ == "__main__":
 		exit(0)
 	else:
 		data.find = data.find.split(',')
+	if data.external == "True":
+		data.external = True
+		display('*', "Crawling on External URLs set to True")
+	else:
+		data.external = crawl_external
 	if data.session_id:
 		headers["Cookie"] = data.session_id
 	if not data.write:
 		data.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}"
-	crawl(data.url, data.find)
+	crawl(data.url, data.find, data.external)
 	print(f"Internal URLs = {len(internal_urls)}")
 	print(f"External URLs = {len(external_urls)}")
 	print(f"\nTotal URLs Extracted = {len(internal_urls) + len(external_urls)}")
